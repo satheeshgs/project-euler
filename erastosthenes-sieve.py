a1ced7742cda1d20edd3aad4fc54bdc09d16a799
satheeshgs <satheesh.gowtham@gmail.com>
2020-08-16 00:05:23 +0530

    Sieve of erastosthenes for problem 10

---
+++ erastosthenes-sieve.py
@@ -0,0 +1,26 @@
+def sieveoferatosthenes(n):
+    #boolean array creation with all values initialized to 1
+    prime=[True for i in range(n+1)]
+    #set the first prime number to a variable p
+    p=2
+    #define the terminating condition (p*p <=n)
+    while(p*p<=n):
+        #starting the iterations and updating multiples
+        if(prime[p]==True):
+            for i in range(p*2,n+1,p):
+                prime[i]=False
+        p+=1
+    prime[0]=False
+    prime[1]=False
+    primearr=[]
+
+    for p in range(n+1):
+        if(prime[p]==True):
+            primearr.append(p)
+
+    #return the prime array values
+    return(primearr)
+
+
+    arr=sieveoferatosthenes(2000000)
+    sum(arr)
